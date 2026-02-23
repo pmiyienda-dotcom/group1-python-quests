c2651964c33108aee5fc4edc13f790a831b4a370
tee687 <t.mtakiwa@alustudent.com>
2026-02-23 01:30:41 -0800

    quest 21 Tendai s commit

---
+++ quests/quest_21_reusable_incantation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def greet_adventurer():
+    """Prints a welcome message"""
+    print("Welcome, brave adventurer, to the Alchemist's Lab!")
+
+# Call the function three times as required
+greet_adventurer()
+greet_adventurer()
+greet_adventurer()
