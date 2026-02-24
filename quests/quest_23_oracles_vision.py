01198b98e55385f62d3e4361f70905df05b9cd75
tee687 <t.mtakiwa@alustudent.com>
2026-02-24 01:56:00 -0800

    quest 23 Tendai s commit

---
+++ quests/quest_23_oracles_vision.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def calculate_area(length, width):
+    return length * width
+
+area1 = calculate_area(5, 10)
+print(f"The area of the first rectangle is: {area1}")
+
+area2 = calculate_area(7, 3)
+print(f"The area of the second rectangle is: {area2}")
